919ecb361b3f75be160361c44ae6ca30c9709bf0
mezyanlahcen88 <mezyan.lahcen88@gmail.com>
2024-01-08 23:41:12 +0100

    updates

--- resources/views/devis/devis_pdf.blade.php
+++ resources/views/devis/devis_pdf.blade.php
@@ -1,193 +1,151 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Invoice #6</title>
-
-    <style>
-        html,
-        body {
-            margin: 10px;
-            padding: 10px;
-            font-family: sans-serif;
-        }
-        h1,h2,h3,h4,h5,h6,p,span,label {
-            font-family: sans-serif;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 0px !important;
-        }
-        table thead th {
-            height: 28px;
-            text-align: left;
-            font-size: 16px;
-            font-family: sans-serif;
-        }
-        table, th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            font-size: 14px;
-        }
-
-        .heading {
-            font-size: 24px;
-            margin-top: 12px;
-            margin-bottom: 12px;
-            font-family: sans-serif;
-        }
-        .small-heading {
-            font-size: 18px;
-            font-family: sans-serif;
-        }
-        .total-heading {
-            font-size: 18px;
-            font-weight: 700;
-            font-family: sans-serif;
-        }
-        .order-details tbody tr td:nth-child(1) {
-            width: 20%;
-        }
-        .order-details tbody tr td:nth-child(3) {
-            width: 20%;
-        }
-
-        .text-start {
-            text-align: left;
-        }
-        .text-end {
-            text-align: right;
-        }
-        .text-center {
-            text-align: center;
-        }
-        .company-data span {
-            margin-bottom: 4px;
-            display: inline-block;
-            font-family: sans-serif;
-            font-size: 14px;
-            font-weight: 400;
-        }
-        .no-border {
-            border: 1px solid #fff !important;
-        }
-        .bg-blue {
-            background-color: #414ab1;
-            color: #fff;
-        }
-    </style>
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="ThemeMarch">
+  <!-- Site Title -->
+  <title>General Invoice</title>
+  <link rel="stylesheet" href="{{URL::asset('invoice/assets/css/style.css')}}">
 </head>
+
 <body>
+  <div class="cs-container">
+    <div class="cs-invoice cs-style1">
+      <div class="cs-invoice_in" id="download_section">
+        <div class="cs-invoice_head cs-type1 cs-mb25">
+          <div class="cs-invoice_left">
+            <p class="cs-invoice_number cs-primary_color cs-mb5 cs-f16"><b class="cs-primary_color">Invoice No:</b> #SM75692</p>
+            <p class="cs-invoice_date cs-primary_color cs-m0"><b class="cs-primary_color">Date: </b>05.01.2022</p>
+          </div>
+          <div class="cs-invoice_right cs-text_right">
+            <div class="cs-logo cs-mb5"><img src="{{URL::asset('assets/images/logo-light.png')}}" alt="Logo"></div>
+          </div>
+        </div>
+        <div class="cs-invoice_head cs-mb10">
+          <div class="cs-invoice_left">
+            <b class="cs-primary_color">Invoice To:</b>
+            <p>
+              Jennifer Richards <br>
+              365 Bloor Street East, Toronto, <br>Ontario, M4W 3L4, <br>
+              Canada
+            </p>
+          </div>
+          <div class="cs-invoice_right cs-text_right">
+            <b class="cs-primary_color">Pay To:</b>
+            <p>
+              Biman Airlines <br>
+              237 Roanoke Road, North York, <br>
+              Ontario, Canada <br>
+              demo@email.com
+            </p>
+          </div>
+        </div>
+        <div class="cs-table cs-style1">
+          <div class="cs-round_border">
+            <div class="cs-table_responsive">
+              <table>
+                <thead>
+                  <tr>
+                    <th class="cs-width_3 cs-semi_bold cs-primary_color cs-focus_bg">Item</th>
+                    <th class="cs-width_4 cs-semi_bold cs-primary_color cs-focus_bg">Description</th>
+                    <th class="cs-width_2 cs-semi_bold cs-primary_color cs-focus_bg">Qty</th>
+                    <th class="cs-width_1 cs-semi_bold cs-primary_color cs-focus_bg">Price</th>
+                    <th class="cs-width_2 cs-semi_bold cs-primary_color cs-focus_bg cs-text_right">Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="cs-width_3">App Development</td>
+                    <td class="cs-width_4">Mobile & Ios Application Development</td>
+                    <td class="cs-width_2">2</td>
+                    <td class="cs-width_1">$460</td>
+                    <td class="cs-width_2 cs-text_right">$920</td>
+                  </tr>
+                  <tr>
+                    <td class="cs-width_3">Ui/UX Design</td>
+                    <td class="cs-width_4">Mobile & Ios Mobile App Design, Product Design</td>
+                    <td class="cs-width_2">1</td>
+                    <td class="cs-width_1">$220</td>
+                    <td class="cs-width_2 cs-text_right">$220</td>
+                  </tr>
+                  <tr>
+                    <td class="cs-width_3">Web Design</td>
+                    <td class="cs-width_4">Web Design & Development</td>
+                    <td class="cs-width_2">2</td>
+                    <td class="cs-width_1">$120</td>
+                    <td class="cs-width_2 cs-text_right">#240</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="cs-invoice_footer cs-border_top">
+              <div class="cs-left_footer cs-mobile_hide">
+                <p class="cs-mb0"><b class="cs-primary_color">Additional Information:</b></p>
+                <p class="cs-m0">At check in you may need to present the credit <br>card used for payment of this ticket.</p>
+              </div>
+              <div class="cs-right_footer">
+                <table>
+                  <tbody>
+                    <tr class="cs-border_left">
+                      <td class="cs-width_3 cs-semi_bold cs-primary_color cs-focus_bg">Subtoal</td>
+                      <td class="cs-width_3 cs-semi_bold cs-focus_bg cs-primary_color cs-text_right">$1140</td>
+                    </tr>
+                    <tr class="cs-border_left">
+                      <td class="cs-width_3 cs-semi_bold cs-primary_color cs-focus_bg">Tax</td>
+                      <td class="cs-width_3 cs-semi_bold cs-focus_bg cs-primary_color cs-text_right">-$20</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="cs-invoice_footer">
+            <div class="cs-left_footer cs-mobile_hide"></div>
+            <div class="cs-right_footer">
+              <table>
+                <tbody>
+                  <tr class="cs-border_none">
+                    <td class="cs-width_3 cs-border_top_0 cs-bold cs-f16 cs-primary_color">Total Amount</td>
+                    <td class="cs-width_3 cs-border_top_0 cs-bold cs-f16 cs-primary_color cs-text_right">$1160</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="cs-note">
+          <div class="cs-note_left">
+            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M416 221.25V416a48 48 0 01-48 48H144a48 48 0 01-48-48V96a48 48 0 0148-48h98.75a32 32 0 0122.62 9.37l141.26 141.26a32 32 0 019.37 22.62z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M256 56v120a32 32 0 0032 32h120M176 288h160M176 368h160" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
+          </div>
+          <div class="cs-note_right">
+            <p class="cs-mb0"><b class="cs-primary_color cs-bold">Note:</b></p>
+            <p class="cs-m0">Here we can write a additional notes for the client to get a better understanding of this invoice.</p>
+          </div>
+        </div><!-- .cs-note -->
+      </div>
+      <div class="cs-invoice_btns cs-hide_print">
+        <a href="javascript:window.print()" class="cs-invoice_btn cs-color1">
+          <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M384 368h24a40.12 40.12 0 0040-40V168a40.12 40.12 0 00-40-40H104a40.12 40.12 0 00-40 40v160a40.12 40.12 0 0040 40h24" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><rect x="128" y="240" width="256" height="208" rx="24.32" ry="24.32" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M384 128v-24a40.12 40.12 0 00-40-40H168a40.12 40.12 0 00-40 40v24" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><circle cx="392" cy="184" r="24"/></svg>
+          <span>Print</span>
+        </a>
+        <button id="download_btn" class="cs-invoice_btn cs-color2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Download</title><path d="M336 176h40a40 40 0 0140 40v208a40 40 0 01-40 40H136a40 40 0 01-40-40V216a40 40 0 0140-40h40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M176 272l80 80 80-80M256 48v288"/></svg>
+          <span>Download</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  <link rel="stylesheet" href="">
 
-    <table class="order-details">
-        <thead>
-            <tr>
-                <th width="50%" colspan="2">
-                    <h2 class="text-start">Funda Ecommerce</h2>
-                </th>
-                <th width="50%" colspan="2" class="text-end company-data">
-                    <span>Invoice Id: #6</span> <br>
-                    <span>Date: 24 / 09 / 2022</span> <br>
-                    <span>Zip code : 560077</span> <br>
-                    <span>Address: #555, Main road, shivaji nagar, Bangalore, India</span> <br>
-                </th>
-            </tr>
-            <tr class="bg-blue">
-                <th width="50%" colspan="2">Order Details</th>
-                <th width="50%" colspan="2">User Details</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Order Id:</td>
-                <td>6</td>
-
-                <td>Full Name:</td>
-                <td>Ved Prakash</td>
-            </tr>
-            <tr>
-                <td>Tracking Id/No.:</td>
-                <td>funda-CRheOqspbA</td>
-
-                <td>Email Id:</td>
-                <td>ved@gmail.com</td>
-            </tr>
-            <tr>
-                <td>Ordered Date:</td>
-                <td>22-09-2022 10:54 AM</td>
-
-                <td>Phone:</td>
-                <td>8889997775</td>
-            </tr>
-            <tr>
-                <td>Payment Mode:</td>
-                <td>Cash on Delivery</td>
-
-                <td>Address:</td>
-                <td>asda asdad asdad adsasd</td>
-            </tr>
-            <tr>
-                <td>Order Status:</td>
-                <td>completed</td>
-
-                <td>Pin code:</td>
-                <td>566999</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <table>
-        <thead>
-            <tr>
-                <th class="no-border text-start heading" colspan="9">
-                    Détails de devis
-                </th>
-            </tr>
-            <tr class="bg-blue">
-                <tr class="text-center">
-                    <th>REF</th>
-                    <th>Désignation</th>
-                    <th>Unir</th>
-                    <th>Quantité</th>
-                    <th>Prix</th>
-                    <th>HT</th>
-                    <th>TVA %</th>
-                    <th>Total tva</th>
-                    <th>TTC total</th>
-                </tr>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td width="10%">16</td>
-                <td>
-                    Mi Note 7
-                </td>
-                <td width="10%">$14000</td>
-                <td width="10%">1</td>
-                <td width="15%" class="fw-bold">$14000</td>
-            </tr>
-            <tr>
-                <td width="10%">17</td>
-                <td>
-                    Vivo V19
-                </td>
-                <td width="10%">$699</td>
-                <td width="10%">1</td>
-                <td width="15%" class="fw-bold">$699</td>
-            </tr>
-            <tr>
-                <td colspan="4" class="total-heading">Total Amount - <small>Inc. all vat/tax</small> :</td>
-                <td colspan="1" class="total-heading">$14699</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <br>
-    <p class="text-center">
-        Thank your for shopping with Funda of Web IT
-    </p>
+  <script src="{{URL::asset('invoice/assets/js/jquery.min.js')}}"></script>
+  <script src="{{URL::asset('invoice/assets/js/jspdf.min.js')}}"></script>
+  <script src="{{URL::asset('invoice/assets/js/html2canvas.min.js')}}"></script>
+  <script src="{{URL::asset('invoice/assets/js/main.js')}}"></script>
 
 </body>
 </html>
