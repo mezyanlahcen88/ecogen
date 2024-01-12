f08f8f784b907f3a03ad571763e294e001542e85
mezyanlahcen88 <mezyan.lahcen88@gmail.com>
2024-01-12 23:37:32 +0100

    updates

--- resources/views/devis/devis_pdf.blade.php
+++ resources/views/devis/devis_pdf.blade.php
@@ -1,151 +1,188 @@
-
 <!DOCTYPE html>
-<html class="no-js" lang="en">
-
+<html>
 <head>
-  <!-- Meta Tags -->
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="ThemeMarch">
-  <!-- Site Title -->
-  <title>General Invoice</title>
-  <link rel="stylesheet" href="{{URL::asset('invoice/assets/css/style.css')}}">
+    <title>How To Generate Invoice PDF In Laravel 9 - Techsolutionstuff</title>
 </head>
-
+<style type="text/css">
+    body{
+        font-family: 'Roboto Condensed', sans-serif;
+    }
+    .m-0{
+        margin: 0px;
+    }
+    .p-0{
+        padding: 0px;
+    }
+    .pt-5{
+        padding-top:5px;
+    }
+    .mt-10{
+        margin-top:10px;
+    }
+    .text-center{
+        text-align:center !important;
+    }
+    .w-100{
+        width: 100%;
+    }
+    .w-50{
+        width:50%;
+    }
+    .w-85{
+        width:85%;
+    }
+    .w-15{
+        width:15%;
+    }
+    .logo img{
+        width:200px;
+        height:60px;
+    }
+    .gray-color{
+        color:#5D5D5D;
+    }
+    .text-bold{
+        font-weight: bold;
+    }
+    .border{
+        border:1px solid black;
+    }
+    table tr,th,td{
+        border: 1px solid #d2d2d2;
+        border-collapse:collapse;
+        padding:7px 8px;
+    }
+    table tr th{
+        background: #F4F4F4;
+        font-size:15px;
+    }
+    table tr td{
+        font-size:13px;
+    }
+    table{
+        border-collapse:collapse;
+    }
+    .box-text p{
+        line-height:10px;
+    }
+    .float-left{
+        float:left;
+    }
+    .total-part{
+        font-size:16px;
+        line-height:12px;
+    }
+    .total-right p{
+        padding-right:20px;
+    }
+</style>
 <body>
-  <div class="cs-container">
-    <div class="cs-invoice cs-style1">
-      <div class="cs-invoice_in" id="download_section">
-        <div class="cs-invoice_head cs-type1 cs-mb25">
-          <div class="cs-invoice_left">
-            <p class="cs-invoice_number cs-primary_color cs-mb5 cs-f16"><b class="cs-primary_color">Invoice No:</b> #SM75692</p>
-            <p class="cs-invoice_date cs-primary_color cs-m0"><b class="cs-primary_color">Date: </b>05.01.2022</p>
-          </div>
-          <div class="cs-invoice_right cs-text_right">
-            <div class="cs-logo cs-mb5"><img src="{{URL::asset('assets/images/logo-light.png')}}" alt="Logo"></div>
-          </div>
-        </div>
-        <div class="cs-invoice_head cs-mb10">
-          <div class="cs-invoice_left">
-            <b class="cs-primary_color">Invoice To:</b>
-            <p>
-              Jennifer Richards <br>
-              365 Bloor Street East, Toronto, <br>Ontario, M4W 3L4, <br>
-              Canada
-            </p>
-          </div>
-          <div class="cs-invoice_right cs-text_right">
-            <b class="cs-primary_color">Pay To:</b>
-            <p>
-              Biman Airlines <br>
-              237 Roanoke Road, North York, <br>
-              Ontario, Canada <br>
-              demo@email.com
-            </p>
-          </div>
-        </div>
-        <div class="cs-table cs-style1">
-          <div class="cs-round_border">
-            <div class="cs-table_responsive">
-              <table>
-                <thead>
-                  <tr>
-                    <th class="cs-width_3 cs-semi_bold cs-primary_color cs-focus_bg">Item</th>
-                    <th class="cs-width_4 cs-semi_bold cs-primary_color cs-focus_bg">Description</th>
-                    <th class="cs-width_2 cs-semi_bold cs-primary_color cs-focus_bg">Qty</th>
-                    <th class="cs-width_1 cs-semi_bold cs-primary_color cs-focus_bg">Price</th>
-                    <th class="cs-width_2 cs-semi_bold cs-primary_color cs-focus_bg cs-text_right">Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class="cs-width_3">App Development</td>
-                    <td class="cs-width_4">Mobile & Ios Application Development</td>
-                    <td class="cs-width_2">2</td>
-                    <td class="cs-width_1">$460</td>
-                    <td class="cs-width_2 cs-text_right">$920</td>
-                  </tr>
-                  <tr>
-                    <td class="cs-width_3">Ui/UX Design</td>
-                    <td class="cs-width_4">Mobile & Ios Mobile App Design, Product Design</td>
-                    <td class="cs-width_2">1</td>
-                    <td class="cs-width_1">$220</td>
-                    <td class="cs-width_2 cs-text_right">$220</td>
-                  </tr>
-                  <tr>
-                    <td class="cs-width_3">Web Design</td>
-                    <td class="cs-width_4">Web Design & Development</td>
-                    <td class="cs-width_2">2</td>
-                    <td class="cs-width_1">$120</td>
-                    <td class="cs-width_2 cs-text_right">#240</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="cs-invoice_footer cs-border_top">
-              <div class="cs-left_footer cs-mobile_hide">
-                <p class="cs-mb0"><b class="cs-primary_color">Additional Information:</b></p>
-                <p class="cs-m0">At check in you may need to present the credit <br>card used for payment of this ticket.</p>
-              </div>
-              <div class="cs-right_footer">
-                <table>
-                  <tbody>
-                    <tr class="cs-border_left">
-                      <td class="cs-width_3 cs-semi_bold cs-primary_color cs-focus_bg">Subtoal</td>
-                      <td class="cs-width_3 cs-semi_bold cs-focus_bg cs-primary_color cs-text_right">$1140</td>
-                    </tr>
-                    <tr class="cs-border_left">
-                      <td class="cs-width_3 cs-semi_bold cs-primary_color cs-focus_bg">Tax</td>
-                      <td class="cs-width_3 cs-semi_bold cs-focus_bg cs-primary_color cs-text_right">-$20</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div class="cs-invoice_footer">
-            <div class="cs-left_footer cs-mobile_hide"></div>
-            <div class="cs-right_footer">
-              <table>
-                <tbody>
-                  <tr class="cs-border_none">
-                    <td class="cs-width_3 cs-border_top_0 cs-bold cs-f16 cs-primary_color">Total Amount</td>
-                    <td class="cs-width_3 cs-border_top_0 cs-bold cs-f16 cs-primary_color cs-text_right">$1160</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-        <div class="cs-note">
-          <div class="cs-note_left">
-            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M416 221.25V416a48 48 0 01-48 48H144a48 48 0 01-48-48V96a48 48 0 0148-48h98.75a32 32 0 0122.62 9.37l141.26 141.26a32 32 0 019.37 22.62z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M256 56v120a32 32 0 0032 32h120M176 288h160M176 368h160" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
-          </div>
-          <div class="cs-note_right">
-            <p class="cs-mb0"><b class="cs-primary_color cs-bold">Note:</b></p>
-            <p class="cs-m0">Here we can write a additional notes for the client to get a better understanding of this invoice.</p>
-          </div>
-        </div><!-- .cs-note -->
-      </div>
-      <div class="cs-invoice_btns cs-hide_print">
-        <a href="javascript:window.print()" class="cs-invoice_btn cs-color1">
-          <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M384 368h24a40.12 40.12 0 0040-40V168a40.12 40.12 0 00-40-40H104a40.12 40.12 0 00-40 40v160a40.12 40.12 0 0040 40h24" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><rect x="128" y="240" width="256" height="208" rx="24.32" ry="24.32" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M384 128v-24a40.12 40.12 0 00-40-40H168a40.12 40.12 0 00-40 40v24" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><circle cx="392" cy="184" r="24"/></svg>
-          <span>Print</span>
-        </a>
-        <button id="download_btn" class="cs-invoice_btn cs-color2">
-          <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Download</title><path d="M336 176h40a40 40 0 0140 40v208a40 40 0 01-40 40H136a40 40 0 01-40-40V216a40 40 0 0140-40h40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M176 272l80 80 80-80M256 48v288"/></svg>
-          <span>Download</span>
-        </button>
-      </div>
+<div class="head-title">
+    <h1 class="text-center m-0 p-0">Devis</h1>
+</div>
+<div class="add-detail mt-10">
+    <div class="w-50 float-left logo mt-10">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW0qFGxyFTY4eI1cmFrFWi5YUT_jkreXwE6Q&usqp=CAU" alt="Logo">
     </div>
-  </div>
-  <link rel="stylesheet" href="">
+    <div class="w-50 float-left mt-10">
+        <p class="m-0 pt-5 text-bold w-100">Devis N°- <span class="gray-color">#DV-1/2023</span></p>
+        <p class="m-0 pt-5 text-bold w-100">Order Id - <span class="gray-color">
+            {{-- {{object->devis_code}} --}}
+        </span></p>
+        <p class="m-0 pt-5 text-bold w-100">Date de devis - <span class="gray-color">22-01-2023</span></p>
+    </div>
 
-  <script src="{{URL::asset('invoice/assets/js/jquery.min.js')}}"></script>
-  <script src="{{URL::asset('invoice/assets/js/jspdf.min.js')}}"></script>
-  <script src="{{URL::asset('invoice/assets/js/html2canvas.min.js')}}"></script>
-  <script src="{{URL::asset('invoice/assets/js/main.js')}}"></script>
-
-</body>
+    <div style="clear: both;"></div>
+</div>
+<div class="table-section bill-tbl w-100 mt-10">
+    <table class="table w-100 mt-10">
+        <tr>
+            <th class="w-50">From</th>
+            <th class="w-50">To</th>
+        </tr>
+        <tr>
+            <td>
+                <div class="box-text">
+                    <p>Mountain View,</p>
+                    <p>California,</p>
+                    <p>United States</p>
+                    <p>Contact: (650) 253-0000</p>
+                </div>
+            </td>
+            <td>
+                <div class="box-text">
+                    <p> 410 Terry Ave N,</p>
+                    <p>Seattle WA 98109,</p>
+                    <p>United States</p>
+                    <p>Contact: 1-206-266-1000</p>
+                </div>
+            </td>
+        </tr>
+    </table>
+</div>
+<div class="table-section bill-tbl w-100 mt-10">
+    <table class="table w-100 mt-10">
+        <tr>
+            <th class="w-50">Payment Method</th>
+            <th class="w-50">Shipping Method</th>
+        </tr>
+        <tr>
+            <td>Cash On Delivery</td>
+            <td>Free Shipping - Free Shipping</td>
+        </tr>
+    </table>
+</div>
+<div class="table-section bill-tbl w-100 mt-10">
+    <table class="table w-100 mt-10">
+        <tr>
+            <th class="w-50">REF</th>
+            <th class="w-50">Désignation & المنتوج</th>
+            <th class="w-50">Unité</th>
+            <th class="w-50">Prix</th>
+            <th class="w-50">Qte</th>
+            <th class="w-50">Montant</th>
+        </tr>
+        <tr align="center">
+            <td>M101</td>
+            <td>Andoid Smart Phone</td>
+            <td>500.2</td>
+            <td>3</td>
+            <td>1500</td>
+            <td>50</td>
+        </tr>
+        <tr align="center">
+            <td>M102</td>
+            <td>Andoid Smart Phone</td>
+            <td>250</td>
+            <td>2</td>
+            <td>500</td>
+            <td>50</td>
+        </tr>
+        <tr align="center">
+            <td>T1010</td>
+            <td>Andoid Smart Phone</td>
+            <td>1000</td>
+            <td>5</td>
+            <td>5000</td>
+            <td>500</td>
+        </tr>
+        <tr>
+            <td colspan="6">
+                <div class="total-part">
+                    <div class="total-left w-85 float-left" align="right">
+                        <p>Sub Total</p>
+                        <p>Tax (18%)</p>
+                        <p>Total Payable</p>
+                    </div>
+                    <div class="total-right w-15 float-left text-bold" align="right">
+                        <p>7600</p>
+                        <p>400</p>
+                        <p>8000.00</p>
+                    </div>
+                    <div style="clear: both;"></div>
+                </div>
+            </td>
+        </tr>
+    </table>
+</div>
 </html>
