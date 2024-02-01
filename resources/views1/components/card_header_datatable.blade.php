3eafa9a8a95a1da08caf34fcc645fc43ad7d7550
unknown <mezyan.lahcen88@gmail.com>
2024-02-01 12:52:00 +0100

    updates

--- resources/views1/components/card_header_datatable.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="card-header">
-    <h4 class="card-title mb-0">{{$card_header_text}}</h4>
-</div><!-- end card header -->
--- resources/views1/tools/form/hidden.blade.php
+++
@@ -1,3 +0,0 @@
-<input class="{{ $field->getFieldClass() }}" {{ $field->getRequired() == true ? 'required' : '' }}
-             type="{{ $field->getType() }}" name="{{ $field->getKey() }}" id="{{ $field->getKey() }}"
-              value="{{ $field->getValue() }}">
